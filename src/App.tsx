7c84c7303567791c68327741af3552f223685638
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-05-01 18:07:26 +0900

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./global.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-[rgb(30,30,30)] w-screen h-screen"></div>
+    </>
+  );
+}
+
+export default App;
