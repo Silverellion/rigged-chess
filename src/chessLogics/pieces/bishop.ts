566cd353c77a7a6910ef89245c88be1850c72fc3
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-05-06 22:53:01 +0900

    Implement bishop

--- src/chessLogics/pieces/bishop.ts
+++ src/chessLogics/pieces/bishop.ts
@@ -1,18 +1,18 @@
-import { Color, Coords, FEN } from "../interface";
+import { FEN, Color, Coords } from "../interface";
 import { Piece } from "./piece";
 
-export class Rook extends Piece {
+export class Bishop extends Piece {
   protected override fen: FEN;
   protected override directions: Coords[] = [
-    { x: 1, y: 0 },
-    { x: -1, y: 0 },
-    { x: 0, y: 1 },
-    { x: 0, y: -1 },
+    { x: 1, y: 1 },
+    { x: -1, y: -1 },
+    { x: -1, y: 1 },
+    { x: 1, y: -1 },
   ];
 
   constructor(color: Color) {
     super(color);
-    color === Color.Black ? (this.fen = FEN.BlackRook) : (this.fen = FEN.WhiteRook);
+    color === Color.Black ? (this.fen = FEN.BlackBishop) : (this.fen = FEN.WhiteBishop);
   }
 
   public override getSlidingMoves(from: Coords, board: FEN[][]): Coords[] {
