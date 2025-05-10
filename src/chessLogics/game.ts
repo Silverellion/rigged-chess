cf8070937ccc4b20ff7bfbf6d522853fd8316945
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-05-10 23:00:11 +0900

    Add SFXs

--- src/chessLogics/game.ts
+++ src/chessLogics/game.ts
@@ -6,6 +6,7 @@ import { Rook } from "./pieces/rook";
 import { Pawn } from "./pieces/pawn";
 import Castling from "./specialMoves/castling";
 import EnPassant from "./specialMoves/enPassant";
+import Sound from "./sound";
 
 export default class Game {
   private board: Board;
@@ -47,30 +48,36 @@ export default class Game {
     const boardState = this.board.getBoard();
     const currentPiece = boardState[fromRow][fromCol];
 
-    if (!currentPiece || currentPiece.getColor() !== this.currentTurn) return false;
+    if (!currentPiece || currentPiece.getColor() !== this.currentTurn) this.playIllegalSFXAndFail();
     let newBoard: Board | null = null;
 
     // Handle castling
     if (currentPiece instanceof King && Math.abs(toCol - fromCol) === 2) {
-      if (toRow !== fromRow) return false;
+      if (toRow !== fromRow) return this.playIllegalSFXAndFail();
       newBoard = Castling.performCastling(this.board, fromCoords, toCoords);
-      if (!newBoard) return false;
+      if (!newBoard) return this.playIllegalSFXAndFail();
+      Sound.castle();
     }
     // Handle en passant
     else if (currentPiece instanceof Pawn && EnPassant.isEnPassantCapture(fromCoords, toCoords, this.board)) {
       newBoard = EnPassant.performEnPassant(this.board, fromCoords, toCoords);
-      if (!newBoard) return false;
+      if (!newBoard) return this.playIllegalSFXAndFail();
+      Sound.capture();
     }
     // Handle regular moves
     else {
       const legalMoves = this.board.getLegalMoves(fromRow, fromCol, this.lastMove);
       const isLegal = legalMoves.some((move) => move.x === toRow && move.y === toCol);
-      if (!isLegal) return false;
+      if (!isLegal) return this.playIllegalSFXAndFail();
 
       const newBoardState = boardState.map((row) => [...row]);
       if (currentPiece instanceof Pawn && !currentPiece.getHasMoved()) currentPiece.setHasMoved();
       if (currentPiece instanceof Rook && !currentPiece.getHasMoved()) currentPiece.setHasMoved();
       if (currentPiece instanceof King && !currentPiece.getHasMoved()) currentPiece.setHasMoved();
+
+      // Check if capturing
+      if (boardState[toRow][toCol]) Sound.capture();
+      else Sound.normalMove();
 
       // Update board state
       newBoardState[toRow][toCol] = currentPiece;
@@ -83,5 +90,10 @@ export default class Game {
     this.currentTurn = this.currentTurn === Color.White ? Color.Black : Color.White;
 
     return true;
+  }
+
+  private playIllegalSFXAndFail(): boolean {
+    Sound.illegalMove();
+    return false;
   }
 }
