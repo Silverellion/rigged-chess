05fcb87b9ac87f6f25467d1787cd4a857dc208dd
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-05-21 21:08:25 +0900

    Adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import "./global.css";
-import Chatbox from "./components/chatbox/Chatbox";
+import Sidebar from "./components/sidebar/Sidebar";
 import Chessboard from "./components/chessboard/Chessboard";
-import Logbox from "./components/logbox/Logbox";
+import Rightbox from "./components/rightbox/Rightbox";
 
 function App() {
   return (
     <>
       <div className="relative w-screen h-screen">
-        <div className="absolute inset-0 bg-[url('./assets/images/background/kasaneTeto.png')] bg-no-repeat bg-cover blur-sm z-0" />
-        <div className="relative z-10 w-full h-full py-10 px-5 grid grid-cols-[1fr_2fr_1fr] gap-4">
-          <Chatbox />
+        <div
+          className="absolute inset-0 
+          bg-[url('./assets/images/background/kasaneTeto.png')] 
+          bg-no-repeat bg-cover blur-sm z-0"
+        />
+        <div className="relative z-10 w-full h-full py-10 px-5 grid grid-cols-[1fr_2fr_2fr] gap-4">
+          <Sidebar />
           <Chessboard />
-          <Logbox />
+          <Rightbox />
         </div>
       </div>
     </>
--- src/components/rightbox/Rightbox.tsx
+++ src/components/rightbox/Rightbox.tsx
@@ -3,10 +3,7 @@ import React from "react";
 const Logbox: React.FC = () => {
   return (
     <>
-      <div
-        className="
-          bg-transparent shadow-[4px_12px_12px_rgba(0,0,0,0.2)]"
-      ></div>
+      <div className="bg-transparent shadow-[4px_12px_12px_rgba(0,0,0,0.2)] grid"></div>
     </>
   );
 };
