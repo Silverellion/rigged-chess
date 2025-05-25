13b8d0d1f304940c968f3b3057df5bb833639bd8
Hoang Tri Minh Tam <hoangtriminhtam@gmail.com>
2025-05-26 02:31:38 +0900

    Implement GET and POST to get stockfish bestmove in backend

--- src/backend/cppCore/main.cpp
+++ src/backend/cppCore/main.cpp
@@ -1,7 +1,16 @@
 #include "processLauncher.h"
+#include "stockfishApiHandler.h"
 #include <iostream>
 
 int main() {
     ProcessLauncher::LaunchEngines();
+
+    std::string getUrl = "http://localhost:1337"; 
+    std::string postUrl = "http://localhost:1337"; 
+    std::string stockfishPath = "C:\\stockfish\\stockfish-windows-x86-64-avx2.exe";
+
+    StockfishApiHandler handler(getUrl, postUrl, stockfishPath);
+    handler.Run();
+
     return 0;
 }
